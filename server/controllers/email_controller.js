ce20185004904038d26f17c5b5e9cdcd5fdf5d6f
Chris Rollins <chris.rollins@leapfire.com>
2017-04-01 20:49:09 -0700

    updated stuff

---
+++ server/controllers/email_controller.js
@@ -0,0 +1,33 @@
+//Controller template
+
+//the following things enable this controller to access the models, and also to send html files as responses
+var path = require("path");
+var htmlPath = path.join(__dirname, "./../../client/");
+var requireFolder = require("./../config/req_folder.js");
+var models = require(path.join(__dirname, "./../config/model_combiner.js"));
+var nodemailer = require("nodemailer");
+
+//when you call a model function it should return a value (usually an array, the result of a query)
+//after that you can make the response here in the controller
+
+module.exports = {
+	index: function(req, res){
+		//response inside callback
+		// models.model_template.test(req, res, function(){
+			res.render('/index.html');
+		// });
+	},
+
+	home: function(req, res){
+		//response inside callback
+		// models.model_template.test(req, res, function(){
+			res.render('/home.html');
+		// });
+	},
+
+	ajax_test: function(req, res){
+		// models.model_template.test(req, res, function(){
+			res.json({ajax_test1 : "ajax_test1", ajax_test2 : "ajax_test2"});
+		// });
+	}
+}
